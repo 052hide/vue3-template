cf87f8d43791b515d498a3df2cbbcc6f333c847a
052hide <hidetoshi1990@gmail.com>
2021-08-21 13:45:37 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="py-4 px-8 border">Sample</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
